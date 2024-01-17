05954327fcee92d33b4412701ba69cecb3016453
Aditya Jasrotia <b322070@iiit-bh.ac.in>
2024-01-17 17:21:40 +0530

    navbar added

--- src/Navbar.js
+++ src/Navbar.js
@@ -18,12 +18,12 @@ function Navbar() {
     <div className={`navbar ${show && "nav_black"}`}>
         <img 
         className='nav_logo'
-        src=''
+        src='7eaf49191bc98d9f3cb80edf484ce2cb.png'
         alt='BingeIt logo'
          />
         <img 
         className='nav_user'
-        src=''
+        src='Netflix-avatar.png'
         alt='BingeIt user'
          />
     </div>
