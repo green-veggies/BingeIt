b2f86e9c1feef84c2447b2a8fe05bfcafa6bade0
Aditya Jasrotia <b322070@iiit-bh.ac.in>
2024-02-05 15:28:16 +0530

    updated

--- src/Row.js
+++ src/Row.js
@@ -1,29 +1,22 @@
-
 //
 
 import React, { useState, useEffect } from "react";
 import axios from "./axios";
-import './Row.css'
-
+import "./Row.css";
 
 const base_url = "https://image.tmdb.org/t/p/original/";
 
-function Row({ title, fetchUrl ,isLargeRow}) {
+function Row({ title, fetchUrl, isLargeRow }) {
   const [movies, setMovies] = useState([]);
-  
 
   useEffect(() => {
     async function fetchData() {
       const request = await axios.get(fetchUrl);
-    //   console.table(request);
+      //   console.table(request);
       setMovies(request.data.results);
     }
     fetchData();
   }, [fetchUrl]);
-
-  
-  
-
 
   return (
     <div className="row">
@@ -32,14 +25,14 @@ function Row({ title, fetchUrl ,isLargeRow}) {
         {movies.map((movie) => (
           <img
             key={movie.id}
-            
-            className={`row__poster ${isLargeRow &&"row__posterLarge"}`}
-            src={`${base_url}${isLargeRow?movie.poster_path:movie.backdrop_path}`}
+            className={`row__poster ${isLargeRow && "row__posterLarge"}`}
+            src={`${base_url}${
+              isLargeRow ? movie.poster_path : movie.backdrop_path
+            }`}
             alt={movie.name}
           />
         ))}
       </div>
-      
     </div>
   );
 }
